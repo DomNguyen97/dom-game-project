184b0129c14c013488f4b25bf6bd7c43206b379a
SeksiBoiDom <ndominic56@gmail.com>
2023-03-20 13:49:08 -0400

    "change with toggle function in js"

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 	/*----- constants -----*/
-let redPiece = 1
-let blackPiece = -1
-let emptySlot = 0
+// let redPiece = 1
+// let blackPiece = -1
+// let emptySlot = 0
 
 	/*----- state variables -----*/
-    let board = [
-        [0, -1, 0, -1, 0, -1, 0, -1, 0, -1],
-        [-1, 0, -1, 0, -1, 0, -1, 0, -1, 0],
-        [0, -1, 0, -1, 0, -1, 0, -1, 0, -1],
-        [-1, 0, -1, 0, -1, 0, -1, 0, -1, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-        [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-      ];
+    // let board = [
+    //     [0, -1, 0, -1, 0, -1, 0, -1, 0, -1],
+    //     [-1, 0, -1, 0, -1, 0, -1, 0, -1, 0],
+    //     [0, -1, 0, -1, 0, -1, 0, -1, 0, -1],
+    //     [-1, 0, -1, 0, -1, 0, -1, 0, -1, 0],
+    //     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    //     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    //     [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    //     [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    //     [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    //     [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    //   ];
 
 	/*----- cached elements  -----*/
 
@@ -24,29 +24,22 @@ let emptySlot = 0
 
 
 	/*----- functions -----*/
-    function builBoard() {
-        game.innerHTML = "";
-        for (let i = 0; i < board.length; i++) {
-          const element = board[i];
-          let row = document.createElement("div"); 
-          row.setAttribute("class", "row");
-          for (let j = 0; j < element.length; j++) {
-            const elmt = element[j];
-            let col = document.createElement("div"); 
-            let piece = document.createElement("div");
-            let caseType = "";
-            let emptySlot = "";
-            if (i % 2 === 0) {
-              if (j % 2 === 0) {
-                caseType = "redCase";
-              } else {
-                caseType = "blackCase";
-              }
-            } else {
-              if (j % 2 !== 0) {
-                caseType = "redCase";
-              } else {
-                caseType = "blackCase";
-              }
-            }
-        }
+    
+    //function turns checkers colors 
+   function toggle(cell) {
+// console.log('toggling cell:', cell)
+// console.log("children: ", cell.children)
+let redChecker = cell.children[0]
+let blackChecker = cell.children[1]
+
+if (!redChecker.hidden && blackChecker.hidden){
+    redChecker.hidden = true
+    blackChecker.hidden = false
+} else if (redChecker.hidden && !blackChecker.hidden) {
+    redChecker.hidden = true
+    blackChecker.hidden = true
+}else if (redChecker.hidden && blackChecker.hidden) {
+    redChecker.hidden = false
+    blackChecker.hidden = true
+}
+   }
