714b10ccc815f9dbf7452480b47ebef28ffe5175
SeksiBoiDom <ndominic56@gmail.com>
2023-03-21 16:51:10 -0400

    "update js"

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // constants
 
 // letters 
-// const letters = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
+// const letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 //  numbers of incorrect guesses
 let tally = 0;
 
